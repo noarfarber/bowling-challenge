4fde5a6f1688cb9ae15e30936d509223f1f38960
Noa Farber <noaros88@gmail.com>
2021-05-09 17:49:25 +0100

    spare game is working, edited prototype

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,26 +1,40 @@
 'use strict';
 
-describe("Game", () => {
+describe("Bowling", () => {
   let game;
 
   beforeEach(() => {
-    game = new Game();
+    game = new Bowling();
   });
 
   let rollAgain = function(pins, rounds) {
-    for(let round = 0; round < rounds; round++) {
+    for (let round = 0; round < rounds; round++) {
       game.roll(pins);
     }
   }
+  
+  describe('gutter game', () => {
+    it('can run a gutter game when the player scores nothing', () => {
+      rollAgain(0, 20);
+      expect(game.scoreBoard()).toEqual(0);
+    })
+  })
+  
+  describe('normal game', () => {
+    it('can count the knocked down pins of a game', () => {
+      rollAgain(1, 20);
+      expect(game.scoreBoard()).toEqual(20);
+    })
+  })
 
-  it('can run a gutter game when the player scores nothing', () => {
-    rollAgain(0, 20);
-    expect(game.scoreBoard()).toEqual(0)
-  });
-
-  it('can count the knocked down pins of a game', () => {
-    rollAgain(1, 20);
-    expect(game.scoreBoard()).toEqual(20)
-  });
-
+  describe('spare game', () => {
+    it('can score a spare round', () => {
+      game.roll(4);
+      game.roll(6);
+      game.roll(2);
+      rollAgain(0, 17);
+      expect(game.scoreBoard()).toEqual(14);
+    })
+  })
+  
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,18 +1,30 @@
 'use strict';
 
-function Game(){
-    this._rolls = [];
-
-  Game.prototype.roll = function(pins){
-    this._rolls.push(pins);
+class Bowling {
+  constructor() {
+    this.rolls = [];
   }
+    roll(pins) {
+      this.rolls.push(pins);
+    };
 
-  Game.prototype.scoreBoard = function(){
-    let counter = 0;
-    for(let round = 0; round < 20; round++) {
-      counter += this._rolls[round]
+    scoreBoard() {
+      let counter = 0;
+      let round = 0;
+  
+      for (let frame = 0; frame < 10; frame++) {
+        if (this.rolls[round] + this.rolls[round + 1] == 10) {
+          counter += this.rolls[round] + this.rolls[round + 1] + this.rolls[round + 2];
+        } else { 
+            counter += this.rolls[round] + this.rolls[round + 1];
+        }
+        round += 2;
+      }
+      return counter;
     }
-    return counter
-  }
+
+
+
+  
 
 }
